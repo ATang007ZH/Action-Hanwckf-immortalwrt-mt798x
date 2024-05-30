00ffd082030c0aa69bf10ac65aa7cf516d1e7e40
ATang007ZH <132867229+ATang007ZH@users.noreply.github.com>
2024-05-30 11:14:19 +0800

    Update diy-part1.sh
    mywatchcat

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git watchcat https://github.com/openwrt/packages/tree/master/utils/watchcat' >>feeds.conf.default
+echo 'src-git watchcat https://github.com/ATang007ZH/mywatchcat/' >>feeds.conf.default
