ca9a1c1817a243db794d425b688f8882608a475c
ATang007ZH <132867229+ATang007ZH@users.noreply.github.com>
2024-05-30 09:37:39 +0800

    Update diy-part2.sh
    replacewatchcat with the latest version

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,5 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+rm -rf feeds/packages/utils/watchcat
+svn co https://github.com/openwrt/packages/trunk/utils/watchcat feeds/packages/utils/watchcat
