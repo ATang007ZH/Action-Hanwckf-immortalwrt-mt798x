4b0c68dd0063b24b9892340aef1bf3724b4e979f
ATang007ZH <132867229+ATang007ZH@users.noreply.github.com>
2024-06-30 09:55:55 +0800

    Update diy-part1.sh
    update passwall package

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git watchcat https://github.com/ATang007ZH/mywatchcat' >>feeds.conf.default
-#echo 'src-git watchcat https://github.com/openwrt/packages/tree/master/utils' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
